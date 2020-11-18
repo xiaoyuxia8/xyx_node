68ddc24d847ba3b669aac450fe2abcc3d212d5f5
xiaoyuxia8 <xiaoyuxia0702@163.com>
2020-11-18 10:49:49 +0800

    修改了包

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const deepClone = function(obj){
-    let result = Array.isArray(obj) ? [] : {};
-    if (obj && typeof obj === "object") {
-      for (let key in obj) {
-        if (obj[key] && typeof obj[key] === "object") {
-          result[key] = deepClone(obj[key]); //如果对象的属性值为object的时候，递归调用deepClone,即在吧某个值对象复制一份到新的对象的对应值中。
-        } else {
-          result[key] = obj[key]; //如果对象的属性值不为object的时候，直接复制参数对象的每一个键值到新的对象对应的键值对中。
-        }
-      }
-      return result;
-    }
-    return obj;
-  }
-export default deepClone
+// module.exports = deepClone = function(obj){
+//     let result = Array.isArray(obj) ? [] : {};
+//     if (obj && typeof obj === "object") {
+//       for (let key in obj) {
+//         if (obj[key] && typeof obj[key] === "object") {
+//           result[key] = deepClone(obj[key]); //如果对象的属性值为object的时候，递归调用deepClone,即在吧某个值对象复制一份到新的对象的对应值中。
+//         } else {
+//           result[key] = obj[key]; //如果对象的属性值不为object的时候，直接复制参数对象的每一个键值到新的对象对应的键值对中。
+//         }
+//       }
+//       return result;
+//     }
+//     return obj;
+//   }
+module.exports = function(){
+    return '你好啊,我的朋友'
+}
